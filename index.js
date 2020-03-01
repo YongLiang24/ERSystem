347bddcb8c9151d009e96770e602d0f8ed2c470b
‘yongliang24’ <yongliang24@gmail.com>
2020-02-29 23:29:45 -0500

    added login card

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 //nav bar logic starts
 let nav01 = document.getElementById("nav01");
 let nav02 = document.getElementById("nav02");
+let empNav = document.getElementById("emp-nav");
+// nav01.style.display="none";
+// nav02.style.display="none";
+// nav01.style.display="block";
+// nav02.style.display="block";
+
+// $("#emp-nav").hide();
+// $("#emp-nav").show();
+
 
     nav01.addEventListener('click', function(){
         console.log("click check script")
-        fetch('http://date.jsontest.com/')
-        .then((response) => {
-            return response.json();
-            })
-        .then((data) => {
-            console.log(data);
-         });
+        // fetch('http://date.jsontest.com/')
+        // .then((response) => {
+        //     return response.json();
+        //     })
+        // .then((data) => {
+        //     console.log(data);
+        //  });
     })
 
     nav02.addEventListener('click', function(){
@@ -26,8 +35,8 @@ let login = document.getElementById("login");
 let loginCard = document.getElementById("login_card");
 
 logout.hidden=true;
-login.hidden=false;
-loginCard.hidden=false;
+login.hidden=true;
+loginCard.hidden=true;
     logout.addEventListener('click', function(){
         console.log(this.id)
     })
@@ -41,3 +50,11 @@ empType.addEventListener('change', function(ev){
     console.log(ev.target.value);
 })
 //login logic ends
+
+//Employee logged in Menu starts
+let firstCard = document.getElementById("first-card");
+let listCards = document.getElementById("list-cards");
+listCards.hidden=true;
+firstCard.addEventListener('click', function(){
+    console.log("clicked card1")
+})
